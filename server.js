c71d8919d2960346df49ed1a9b975df8a76dd79f
Lucio Paiva <luciopaiva@gmail.com>
2018-10-01 18:34:27 -0300

    Add some comments

--- server.js
+++ server.js
@@ -1,3 +1,12 @@
+/*
+ * Sample restify server that also accepts socket.io connections.
+ *
+ * This example shows how to:
+ *
+ * - serve some API via Restify
+ * - serve static files via Restify
+ * - receive socket.io connection requests and reply with asynchronous messages (unicast and broadcast)
+ */
 
 const
     fs = require("fs"),
@@ -18,20 +27,25 @@ server.get("/random", (req, res) => res.send({ value: (Math.random() * 1000).toF
 // serve client-side socket.io script
 server.get("/socket.io.js", (req, res) => fs.createReadStream(PATH_TO_CLIENT_SIDE_SOCKET_IO_SCRIPT).pipe(res));
 
+// serve static files under /public
 server.get("/*", restify.plugins.serveStatic({
     directory: __dirname + "/public",
     default: "index.html",
 }));
 
+// handle socket.io clients connecting to us
 io.sockets.on("connect", socket => {
     clientsOnline.add(socket);
     io.emit("clients-online", clientsOnline.size);
+
+    // handle client disconnect
     socket.on("disconnect", () => {
         clientsOnline.delete(socket);
         io.emit("clients-online", clientsOnline.size);
     })
 });
 
+// send regular messages to all socket.io clients with the current server time
 setInterval(() => clientsOnline.size > 0 && io.emit("server-time", (new Date()).toISOString()), 100);
 
 server.listen(SERVER_PORT, "0.0.0.0", () => console.log(`Listening at ${server.url}`));
